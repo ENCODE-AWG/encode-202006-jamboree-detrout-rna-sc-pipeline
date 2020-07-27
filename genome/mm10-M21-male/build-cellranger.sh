ec0f6d590435aded63b063b6cb77ea7bb1bca746
Diane Trout <diane@ghic.org>
2020-07-27 13:49:03 -0700

    Update paths for woldlab server

--- genome/mm10-M21-male/build-cellranger.sh
+++ genome/mm10-M21-male/build-cellranger.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
-export CELLRANGER_DIR=~/cellranger-3.1.0
+export CELLRANGER_DIR=~/proj/illumina/cellranger-3.1.0
 export PATH=$CELLRANGER_DIR:$PATH
 
 export GENOME_ROOT=~/proj/genome
+export ENCODE_ROOT=~/proj/encode-202006-jamboree-detrout-rna-sc-pipeline/genome
 export GENOME=mm10
 export ANNOTATION=M21
 export SEX=male
 export GENOME_TRIPLET=${GENOME}-${ANNOTATION}-${SEX}
 export GENOME_DIR=${GENOME_ROOT}/${GENOME_TRIPLET}/
 export GENOME_FASTA=${GENOME_DIR}mm10-M21-ercc+phix.fa
-export GTF=${GENOME_DIR}gencode.vM21-tRNAs-ERCC.gff
+#export GTF=${GENOME_DIR}gencode.vM21-tRNAs-ERCC.gff
+export GTF=${ENCODE_ROOT}/mm10-M21-male/gencode.vM21-tRNAs-exon-ERCC.gff
 
-export THREADS=32
+export THREADS=20
 export MEMORY=60
 
 cellranger mkref \
