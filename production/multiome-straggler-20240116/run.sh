3530c9bf65d613f2b63ef7dcec85eb5cb937da8d
Diane Trout <diane@ghic.org>
2024-02-26 20:17:20 -0800

    Import settings for another few straggling datasets

---
+++ production/multiome-straggler-20240116/run.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+pushd ENCLB115WMH
+# snakemake --use-singularity --snakefile ~/proj/woldlab-rna-seq/workflow/process-encode-10x.snakefile --cores 40 
+popd
+pushd ENCLB610VVE
+ snakemake --use-singularity --snakefile ~/proj/woldlab-rna-seq/workflow/process-encode-10x.snakefile --cores 40 
+popd
+pushd ENCLB867TXT
+ snakemake --use-singularity --snakefile ~/proj/woldlab-rna-seq/workflow/process-encode-10x.snakefile --cores 40 
+popd
+pushd ENCLB589DNC
+ snakemake --use-singularity --snakefile ~/proj/woldlab-rna-seq/workflow/process-encode-10x.snakefile --cores 40 
+popd
+pushd ENCLB119RYO
+ snakemake --use-singularity --snakefile ~/proj/woldlab-rna-seq/workflow/process-encode-10x.snakefile --cores 40 
+popd
+pushd ENCLB460GQW
+ snakemake --use-singularity --snakefile ~/proj/woldlab-rna-seq/workflow/process-encode-10x.snakefile --cores 40 
+popd
+pushd ENCLB895XTF
+ snakemake --use-singularity --snakefile ~/proj/woldlab-rna-seq/workflow/process-encode-10x.snakefile --cores 40 
+popd
+pushd ENCLB942EHW
+ snakemake --use-singularity --snakefile ~/proj/woldlab-rna-seq/workflow/process-encode-10x.snakefile --cores 40 
+popd
+pushd ENCLB953VEF
+ snakemake --use-singularity --snakefile ~/proj/woldlab-rna-seq/workflow/process-encode-10x.snakefile --cores 40 
+popd
